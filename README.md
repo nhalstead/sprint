d4c75818ed2c6a26da020de0c203447275e0ab1f
Noah Halstead <nhalstead@users.noreply.github.com>
2018-10-28 22:25:21 -0400

    Update to Fix issue #1 and Add more options
    This is commit fixes #1 by adding graceful exits of errors and more HASH output types.
    
    In this update you will now have to specify the domain and what SHA version you want back.
    
    New Changes:
    If you do not provide a domain it will connect to Localhost.
    If not SHA Version is specified it will return nothing with an Exit code of 1.
    If you specify more than one SHA hash version the Hashes are separated with `,` "CSV" list.
    
    All of the CLI Flags that can be provided are as follows:
    -sha1             Export the Sha1 Version of the Cert Fingerprint
    -sha256         Export the Sha256 Version of the Cert Fingerprint
    -sha512         Export the Sha512 Version of the Cert Fingerprint
    -disableNth   Does not insert `:` into the output
    -domain        The Domain to get the Fingerprint from.

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # SSL Fingerprint
 
-This cli application written in go will connect to the given domain and return the `sha1` fingerprint formatted appropriately.
+This cli application written in go will connect to the given domain and return the `sha1`/`sha256`/`sha512` fingerprint formatted appropriately.
 
 Simple, Fast, Easy
-
 
 ### Why?
 > You can use this package in some automation tasks like cert
@@ -16,12 +15,30 @@ Simple, Fast, Easy
 >  with this by using an if Statement.
 
 ### How to use:
-> go run src/fingerprint.go [domain]
+> go run src/fingerprint.go -domain [domain] -sha1
+
+To use SHA256 or SHA512 simply use the `-sha256` and `-sha512` flag.
+> go run src/fingerprint.go -domain [domain] -sha256
+
+> go run src/fingerprint.go -domain [domain] -sha512
 
 ### Example Usage
-> go run src/fingerprint.go google.com
+> go run src/fingerprint.go google.com -sha1
 >
->E3:CF:06:92:EF:33:E6:35:17:34:E5:6E:35:81:52:83:90:06:E1:B7
+> 2B:AE:50:AF:6A:71:43:08:F1:98:A8:23:8A:1E:3A:1A:D2:19:F3:2B
+
+### Advanced Usage and Extras
+If you want SHA1 and SHA512 in one request you can combine it into one request
+> go run src/fingerprint.go -domain google.com -sha1 -sha512
+>
+> 2B:AE:50:AF:6A:71:43:08:F1:98:A8:23:8A:1E:3A:1A:D2:19:F3:2B,E8:28: ... :47:4F:5B
+
+To remove the `:` characters from the hash output you can add the flag of `-disableNth` and it will not insert them.
+> go run src/fingerprint.go google.com -sha1
+>
+> 2BAE50AF6A714308F198A8238A1E3A1AD219F32B
+
+**NOTE: If no domain is defined using the Flag, It uses `localhost` as the domain.**
 
 # Contributors
 - nhalstead @ https://github.com/nhalstead
--- src/fingerprint.go
+++ src/fingerprint.go
@@ -11,6 +11,9 @@ package main
 import (
 	"bytes"
 	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -18,16 +21,59 @@ import (
 	"strings"
 )
 
-func main() {
-	domainURL := os.Args[1]
-	fmt.Print(getFingerprint(domainURL))
+var sha1Export = flag.Bool("sha1", false, "Output SHA1")
+var sha256Export = flag.Bool("sha256", false, "Output SHA256")
+var sha512Export = flag.Bool("sha512", false, "Output SHA512")
+var disableNth = flag.Bool("disableNth", false, "Do not insert `:` into the output hash")
+var domainURL = flag.String("domain", "https://localhost", "The Domain to get the SSL Cert Fingerprint from, Default is Localhost")
 
+func main() {
+	flag.Parse()
+	domainURL := *domainURL
+
+	// Exit if none of the Hashes are Selected.
+	if *sha1Export == false && *sha256Export == false && *sha512Export == false {
+		os.Exit(1)
+	}
+
+	// If the URL is HTTP, Do not process it as it does NOT have SSL Cert we are looking for.
+	if !strings.HasPrefix(domainURL, "http://") {
+		hashList := getFingerprint(domainURL)
+
+		// Used to insert `,` between the hashes if multible are selected.
+		alreadyExportedOnce := false
+
+		// SHA1 Hash Export
+		if *sha1Export == true {
+			alreadyExportedOnce = true
+			fmt.Print(hashList[0]) // SHA1 Hash
+		}
+
+		// SHA256 Hash Export
+		if *sha256Export == true {
+			if alreadyExportedOnce == true {
+				fmt.Print(",")
+			}
+			alreadyExportedOnce = true
+			fmt.Print(hashList[1]) // SHA256 Hash
+		}
+
+		// SHA512 Hash Export
+		if *sha512Export == true {
+			if alreadyExportedOnce == true {
+				fmt.Print(",")
+			}
+			fmt.Print(hashList[2]) // SHA512 Hash
+		}
+
+	}
 }
 
-func getFingerprint(s string) string {
+func getFingerprint(s string) []string {
 
 	urlData, _ := url.Parse(s)
 	reqURL := urlData.Host
+	hashes := []string{"", "", ""} // Init Blank Values
 
 	// This will handle if the client does not have the url and its just the domain name.
 	if reqURL == "" {
@@ -35,21 +81,38 @@ func getFingerprint(s string) string {
 	}
 
 	req, _ := http.NewRequest("HEAD", "https://"+reqURL, bytes.NewBufferString(""))
-	req.Header.Set("User-Agent", "GoFingerprint/1.0.1")
+	req.Header.Set("User-Agent", "GoFingerprint/1.0.2")
 
 	resp, _ := http.DefaultClient.Do(req)
 
-	tls := resp.TLS
-	if tls != nil {
-		// Try the first one for simplicity
-		cert := tls.PeerCertificates[0]
+	// To handle the situations where the connection fails.
+	if resp != nil {
+		tls := resp.TLS
+		if tls != nil {
+			// Try the first one for simplicity
+			cert := tls.PeerCertificates[0]
 
-		fingerprint := strings.ToUpper(string([]byte(fmt.Sprintf("%x", sha1.Sum(cert.Raw)))))
-		fingerprint = insertNth(fingerprint, 2)
-		return fingerprint
-		// Do something with the signature
+			fingerprintSHA1 := strings.ToUpper(string([]byte(fmt.Sprintf("%x", sha1.Sum(cert.Raw)))))
+			if *disableNth == false {
+				fingerprintSHA1 = insertNth(fingerprintSHA1, 2)
+			}
+			hashes[0] = fingerprintSHA1
+
+			fingerprintSHA256 := strings.ToUpper(string([]byte(fmt.Sprintf("%x", sha256.Sum256(cert.Raw)))))
+			if *disableNth == false {
+				fingerprintSHA256 = insertNth(fingerprintSHA256, 2)
+			}
+			hashes[1] = fingerprintSHA256
+
+			fingerprintSHA512 := strings.ToUpper(string([]byte(fmt.Sprintf("%x", sha512.Sum512(cert.Raw)))))
+			if *disableNth == false {
+				fingerprintSHA512 = insertNth(fingerprintSHA512, 2)
+			}
+			hashes[2] = fingerprintSHA512
+		}
 	}
-	return ""
+
+	return hashes
 }
 
 /**
